2963b012951dc8d0f8b753918f607ef483a9aec4
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-07-14 14:13:26 +0300

    Implemented responsive design for Add Car, Add Motorcycle, Vehicle Profile, and Vehicles pages.

--- src/main/resources/static/js/blinker.js
+++
@@ -1,9 +0,0 @@
-let heroBg = document.querySelector("body");
-
-setInterval(() => {
-    heroBg.style.backgroundImage = "url(/images/car-light.jpg)";
-
-    setTimeout(() => {
-        heroBg.style.backgroundImage = "url(/images/car.jpg)";
-    }, 1000);
-}, 2200);
