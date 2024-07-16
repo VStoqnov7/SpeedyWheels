2b7e798b1d9abf343e05ea391c0a5a8509b7aef9
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-07-17 01:39:37 +0300

    Added additional methods to UserRepository for user queries.

--- src/main/java/com/example/speedywheels/repository/UserRepository.java
+++ src/main/java/com/example/speedywheels/repository/UserRepository.java
@@ -4,9 +4,13 @@ import com.example.speedywheels.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByContactEmail(String email);
 
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
