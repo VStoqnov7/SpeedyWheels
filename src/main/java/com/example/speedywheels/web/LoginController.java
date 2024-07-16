1756c7c4f4ee5beb5bd0f1379d160bc76b34bbd6
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-07-17 01:42:05 +0300

    Added handling for login failure.

--- src/main/java/com/example/speedywheels/web/LoginController.java
+++ src/main/java/com/example/speedywheels/web/LoginController.java
@@ -2,6 +2,7 @@ package com.example.speedywheels.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,4 +15,13 @@ public class LoginController {
         model.setViewName("login");
         return model;
     }
+
+    @PostMapping("/login-error")
+    public ModelAndView onLoginFailure(ModelAndView modelAndView) {
+
+        modelAndView.addObject("bad_credentials", true);
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
 }
